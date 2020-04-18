b01be4696a319c18511f34e71e69f82ca9da2a06
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-04-18 18:50:52 +0530

    export types

--- src/MakieTeX.jl
+++ src/MakieTeX.jl
@@ -11,6 +11,7 @@ include("rendering.jl")
 include("recipe.jl")
 include("layoutable.jl")
 
+export TeXDocument, CachedTeX
 export dvi2svg, latex2dvi, rsvg2recordsurf, svg2rsvg
 export teximg, teximg!, TeXImg
 export LTeX
