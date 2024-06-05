919911507639157b45da9f20b67c717daeb7f732
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-06-05 12:25:01 -0400

    Correct docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,11 +4,12 @@ using MakieTeX
 
 makedocs(;
     modules=[MakieTeX],
-    format=DocumenterVitepress.MarkdownVitepress(; repo = "https://github.com/JuliaPlots/MakieTeX.jl"),
+    format=DocumenterVitepress.MarkdownVitepress(; 
+        repo = "https://github.com/JuliaPlots/MakieTeX.jl"
+    ),
     pages=[
         "Home" => "index.md",
         "API reference" => "api.md",
-
     ],
     edit_link="https://github.com/JuliaPlots/MakieTeX.jl/blob/{commit}{path}#L{line}",
     sitename="MakieTeX.jl",
