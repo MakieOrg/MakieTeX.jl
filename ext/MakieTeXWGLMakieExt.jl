6acea5a20f79e9b8aa7dd5cb993983d012e29bb0
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-06-05 12:20:30 -0400

    Comment out some lines in the (outdated) extensions

--- ext/MakieTeXWGLMakieExt.jl
+++ ext/MakieTeXWGLMakieExt.jl
@@ -3,7 +3,7 @@ module MakieTeXWGLMakieExt
 using WGLMakie, MakieTeX
 
 WGLMakie.wgl_convert(ct::AbstractCachedDocument) = MakieTeX.rasterize(ct, #=scale=#1)
-WGLMakie.wgl_convert((ct::AbstractCachedDocument, scale::Real)) = MakieTeX.rasterize(ct, scale)
+# WGLMakie.wgl_convert((ct::AbstractCachedDocument, scale::Real)) = MakieTeX.rasterize(ct, scale)
 
 
 end
