2374a7a6db5f0e8a9f1942eef21f03de61d1d4ee
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-06-05 14:11:47 -0400

    comment out all extension code

--- ext/MakieTeXGLMakieExt.jl
+++ ext/MakieTeXGLMakieExt.jl
@@ -4,7 +4,7 @@ This extension tells GLMakie how to rasterize a cached document, if it is requir
 =#
 using GLMakie, MakieTeX
 
-GLMakie.GLAbstraction.gl_convert(ct::MakieTeX.AbstractCachedDocument) = MakieTeX.rasterize(ct, #=scale=#1)
+# GLMakie.GLAbstraction.gl_convert(ct::MakieTeX.AbstractCachedDocument) = MakieTeX.rasterize(ct, #=scale=#1)
 # GLMakie.GLAbstraction.gl_convert((ct::MakieTeX.AbstractCachedDocument, scale::Real)) = MakieTeX.rasterize(ct, scale)
 
 end
--- ext/MakieTeXWGLMakieExt.jl
+++ ext/MakieTeXWGLMakieExt.jl
@@ -2,7 +2,7 @@ module MakieTeXWGLMakieExt
 
 using WGLMakie, MakieTeX
 
-WGLMakie.wgl_convert(ct::AbstractCachedDocument) = MakieTeX.rasterize(ct, #=scale=#1)
+# WGLMakie.wgl_convert(ct::AbstractCachedDocument) = MakieTeX.rasterize(ct, #=scale=#1)
 # WGLMakie.wgl_convert((ct::AbstractCachedDocument, scale::Real)) = MakieTeX.rasterize(ct, scale)
 
 
