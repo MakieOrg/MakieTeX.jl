00c9c42993cae6e599e516a35acb0921b9aed9df
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2026-05-19 10:34:32 +0200

    test: Rename gate to SKIP_GLMAKIE; use plain conditional using

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,8 +10,11 @@ using tectonic_jll  # activates MakieTeXLaTeXExt
 # GLMakie needs a GPU; Windows / macOS GitHub-hosted runners don't have one,
 # so skip the GL reftests there. Linux CI has xvfb. Locally everyone has a
 # GPU. The CI workflow Pkg.rm's GLMakie on Windows / macOS to match.
-const HAS_GLMAKIE = !(get(ENV, "CI", "false") == "true" && (Sys.isapple() || Sys.iswindows()))
-HAS_GLMAKIE && @eval using GLMakie
+const SKIP_GLMAKIE = get(ENV, "CI", "false") == "true" && (Sys.isapple() || Sys.iswindows())
+
+if !SKIP_GLMAKIE
+    using GLMakie
+end
 
 include("reference_tests.jl")
 
@@ -19,7 +22,7 @@ include("reference_tests.jl")
     @testset "CairoMakie" begin
         run_reftests(:CairoMakie)
     end
-    if HAS_GLMAKIE
+    if !SKIP_GLMAKIE
         @testset "GLMakie" begin
             run_reftests(:GLMakie)
         end
